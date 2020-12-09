82b8a62cc2830ade21e25984e94c2854378b61d1
NouemanKHAL <noueman.khal@gmail.com>
2020-12-09 13:42:25 +0100

    Update firebase.js
    fixed code indentation

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-  import firebase from "firebase";
+import firebase from "firebase";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBitVPrwdJyJIcdB68SmFlTsOSOfoLiahc",
-    authDomain: "clon-97460.firebaseapp.com",
-    databaseURL: "https://clon-97460.firebaseio.com",
-    projectId: "clon-97460",
-    storageBucket: "clon-97460.appspot.com",
-    messagingSenderId: "319383989331",
-    appId: "1:319383989331:web:c8016c016c780200e66aa6",
-    measurementId: "G-BS52RYG1YF"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBitVPrwdJyJIcdB68SmFlTsOSOfoLiahc",
+  authDomain: "clon-97460.firebaseapp.com",
+  databaseURL: "https://clon-97460.firebaseio.com",
+  projectId: "clon-97460",
+  storageBucket: "clon-97460.appspot.com",
+  messagingSenderId: "319383989331",
+  appId: "1:319383989331:web:c8016c016c780200e66aa6",
+  measurementId: "G-BS52RYG1YF"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
